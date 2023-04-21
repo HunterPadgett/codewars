07fa9e4fa52669739233da1704ac1766ea97b61c
Hunter Padgett <gizmokabanjo@gmail.com>
2023-04-21 11:30:06 -0400

    cw of the day

---
+++ 4-21-23.js
@@ -0,0 +1,14 @@
+// 7 kyu
+// Remove anchor from URL
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url) {
+ if (!url.includes("#")) return url;
+ const anchor = url.indexOf("#");
+ return url.slice(0, anchor);
+}
